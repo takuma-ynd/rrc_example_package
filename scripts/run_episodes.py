9e5dd1c02af9bb26eb3da96b87d9ce9a0c49adf9
Takuma Yoneda <takuma-ynd@users.noreply.github.com>
2020-10-16 16:04:17 +0900

    add some files

---
+++ scripts/run_episodes.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import numpy as np
+from code.training_env import make_training_env
+from code.training_env.env import ActionType
+import move_cube
+import cube_env
+
+# env = make_training_env(visualization=False, **eval_config)
+# env.unwrapped.initializer = initializer
+
+# eval_config = {
+#         'action_space': 'torque_and_position',
+#         'frameskip': 3,
+#         'residual': True,
+#         'reward_fn': f'task{difficulty}_competition_reward',
+#         'termination_fn': 'no_termination',
+#         'initializer': f'task{difficulty}_init',
+#         'monitor': False,
+#         'rank': 0
+# }
+
+goal = move_cube.sample_goal(3)
+goal_dict = {
+    'position': move_cube.position,
+    'orientation': move_cube.orientation
+}
+
+env = cube_env.RealRobotCubeEnv(goal_dict, 3)
+
+obs = env.reset()
+done = False
+accumulated_reward = 0
+if env.action_type == ActionType.TORQUE_AND_POSITION:
+        zero_action = {
+        'torque': (env.action_space['torque'].sample() * 0).astype(np.float64),
+        'position': (env.action_space['position'].sample() * 0).astype(np.float64)
+        }
+        assert zero_action['torque'].dtype == np.float64
+        assert zero_action['position'].dtype == np.float64
+else:
+        zero_action = np.array(env.action_space.sample() * 0).astype(np.float64)
+        assert zero_action.dtype == np.float64
+
+while not done:
+        obs, reward, done, info = env.step(zero_action)
+        accumulated_reward += reward
+print("Accumulated reward: {}".format(accumulated_reward))
